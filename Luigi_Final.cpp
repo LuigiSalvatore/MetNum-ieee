70239ef93cc63bfd3f2ad14f479596104220e7d8
LuigiSalvatore <lumikich@gmail.com>
2024-03-12 18:52:56 -0300

    final Luigi_Final.cpp

--- Luigi_Final.cpp
+++ Luigi_Final.cpp
@@ -6,8 +6,72 @@
 
 using namespace std;
 
-unsigned flags[5] = {FE_DIVBYZERO, FE_INEXACT, FE_INVALID, FE_OVERFLOW, FE_UNDERFLOW};
-string flagstr[5] = {"FE_DIVBYZERO", "FE_INEXACT", "FE_INVALID", "FE_OVERFLOW", "FE_UNDERFLOW"};
+string bitConversion(const float f);
+string flags_str();
+
+const unsigned flags[5] = {FE_DIVBYZERO, FE_INEXACT, FE_INVALID, FE_OVERFLOW, FE_UNDERFLOW};
+const string flagstr[5] = {"FE_DIVBYZERO", "FE_INEXACT", "FE_INVALID", "FE_OVERFLOW", "FE_UNDERFLOW"};
+
+int main()
+{
+    float f[3] = {0, 0, 0};
+    string s[2], op;
+    cin >> s[0] >> op >> s[1];
+    stringstream ss;
+    feclearexcept(FE_ALL_EXCEPT);
+
+    for (int i = 0; i < 2; i++)
+        f[i] = stof(s[i]);
+
+    switch (op[0])
+    {
+    case '+':
+        f[2] = f[0] + f[1];
+        break;
+    case '-':
+        f[2] = f[0] - f[1];
+        break;
+    case 'x':
+        f[2] = f[0] * f[1];
+        break;
+    case '*':
+        f[2] = f[0] * f[1];
+        break;
+    case 'X':
+        f[2] = f[0] * f[1];
+        break;
+    case '/':
+        f[2] = f[0] / f[1];
+        break;
+    default:
+        cout << "Invalid operator";
+        return 0;
+    }
+    for (int i = 0; i < 3; i++)
+        ss << bitConversion(f[i]);
+    cout
+        << ss.str()
+        << endl
+        << flags_str();
+
+    return 0;
+}
+
+string bitConversion(const float f)
+{
+    stringstream ss;
+    unsigned int UI = *(unsigned int *)&f;
+    ss
+        << bitset<1>((UI >> 31))
+        << " "
+        << bitset<8>((UI >> 23))
+        << " "
+        << bitset<23>(UI)
+        << "= "
+        << f
+        << endl;
+    return ss.str();
+}
 
 string flags_str()
 {
@@ -19,84 +83,4 @@ string flags_str()
         ss << flagstr[i] << " = " << flag << endl;
     }
     return ss.str();
-}
-
-int main()
-{
-    float n1f, n2f, res;
-    string n1, op, n2;
-    cin >> n1 >> op >> n2;
-    int flag;
-    unsigned int imfuckingtired;
-    stringstream ss;
-
-    feclearexcept(FE_ALL_EXCEPT);
-    n1f = stof(n1);
-    n2f = stof(n2);
-
-    switch (op[0])
-    {
-    case '+':
-        res = n1f + n2f;
-        break;
-    case '-':
-        res = n1f - n2f;
-        break;
-    case 'x':
-        res = n1f * n2f;
-        break;
-    case '*':
-        res = n1f * n2f;
-        break;
-    case 'X':
-        res = n1f * n2f;
-        break;
-    case '/':
-        res = n1f / n2f;
-        break;
-    default:
-        cout << "Invalid operator";
-        return 0;
-    }
-
-#pragma region BitConversion
-    imfuckingtired = *(unsigned int *)&n1f;
-    ss
-        << bitset<1>((imfuckingtired >> 31))
-        << " "
-        << bitset<8>((imfuckingtired >> 23))
-        << " "
-        << bitset<23>(imfuckingtired)
-        << "= "
-        << n1f
-        << endl;
-
-    imfuckingtired = *(unsigned int *)&n2f;
-    ss
-        << bitset<1>((imfuckingtired >> 31))
-        << " "
-        << bitset<8>((imfuckingtired >> 23))
-        << " "
-        << bitset<23>(imfuckingtired)
-        << "= "
-        << n2f
-        << endl;
-
-    imfuckingtired = *(unsigned int *)&res;
-    ss
-        << bitset<1>((imfuckingtired >> 31))
-        << " "
-        << bitset<8>((imfuckingtired >> 23))
-        << " "
-        << bitset<23>(imfuckingtired)
-        << "= "
-        << res
-        << endl;
-#pragma endregion
-    cout
-        << ss.str()
-        << endl
-        << flags_str();
-
-    return 0;
 }
